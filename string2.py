3f49f8830086b0821515575ef02ad7a0492b9c70
aakashp2000 <aakashp2000@gmail.com>
2022-02-15 13:24:40 +0530

    Arrange string characters such that lowercase letters should come first

---
+++ string2.py
@@ -0,0 +1,12 @@
+#Arrange string characters such that lowercase letters should come first
+str1="AaKaaSH"
+print("Orignial Strings:",str1)
+lower=[]
+upper=[]
+for char in str1:
+    if char.islower():
+        lower.append(char)
+    else:
+        upper.append(char)
+sorted_str=''.join(lower+upper)
+print('Result:',sorted_str)
